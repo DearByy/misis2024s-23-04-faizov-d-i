96e3d93e4ae29fc7a1458943501ee596bf157f30
DearByy <145282579+DearByy@users.noreply.github.com>
2024-05-10 19:35:51 +0300

    Update complex.hpp

--- prj.lab/complex/complex.hpp
+++ prj.lab/complex/complex.hpp
@@ -1,61 +1,75 @@
-#ifndef COMPLEX_COMPLEX_HPP
-#define COMPLEX_COMPLEX_HPP
+#ifndef Complex_2024
+#define Complex_2024
 
 #include <iostream>
 #include <sstream>
+#include <limits>
+static const double eps = 2 * std::numeric_limits<double>::epsilon();
+
 
 struct Complex {
+    double re{ 0.0 };   //тоже самое, что double re = 0.0;
+    double im{ 0.0 };  
 
-	~Complex() = default;
-	Complex(const Complex&) = default;
-	Complex() = default;
-	Complex(const double real) : re(real) {}	
-	Complex(const double real, const double imaginary) : re(real), im(imaginary) {}
-	Complex operator=(const double rhs) { return operator=(Complex(rhs)); }
-	bool operator==(const Complex& rhs) const { return (re == rhs.re) && (im == rhs.im); }
-	bool operator!=(const Complex& rhs) const { return !(operator==(rhs)); }
-	Complex& operator+=(const Complex& rhs);
-	Complex& operator+=(const double rhs) { return operator+=(Complex(rhs)); }
-	Complex& operator-=(const Complex& rhs);
-	Complex& operator-=(const double rhs) { return operator-=(Complex(rhs)); }
-	Complex& operator*=(const Complex& rhs);
-	Complex& operator*=(const double& rhs);
-	Complex& operator/=(const Complex& rhs);
-	Complex& operator/=(const double& rhs);
-	std::ostream& writeTo(std::ostream& ostrm) const;
-	std::istream& readFrom(std::istream& istrm);
+    static const char leftBrace{ '{' };  
+    static const char separator{ ',' };   
+    static const char rightBrace{ '}' };  
+  
+    Complex() = default;
+    Complex(const Complex&) = default;
+    explicit Complex(const double real) : re(real) {} //если число имеет только веществ. часть
+    Complex(const double real, const double imaginary) : re(real), im(imaginary) {}
+    ~Complex() = default;
 
-	double re{ 0.0 };
-	double im{ 0.0 };
 
-	static const char leftBrace{ '{' };
-	static const char separator{ ',' };
-	static const char rightBrace{ '}' };
 
-	
+    bool operator==(const Complex& rhs) {
+        return ((std::abs(re - rhs.re) < eps) && (std::abs(im - rhs.im) < eps)); 
+    }
+    
+    
+    Complex& operator+=(const Complex& rhs);
+    Complex& operator+=(const double rhs) {
+        return operator+=(Complex(rhs));
+    }
+    Complex& operator-=(const Complex& rhs);
+    Complex& operator-=(const double rhs) {
+        return operator-=(Complex(rhs));
+    }
+    //объявили внутри структуры, опеределим вне
+    Complex& operator*=(const Complex& rhs);
+    Complex& operator*=(const double& rhs);
+    Complex& operator/=(const Complex& rhs);
+    Complex& operator/=(const double& rhs);
 
+
+    std::ostream& writeTo(std::ostream& ostrm) const;
+    std::istream& readFrom(std::istream& istrm);
 
 };
 
-//Все ниже определенно в спп, причем находится в привате
-
-bool operator==(const double& lhs, const Complex& rhs);
-bool operator==(const Complex& lhs, const double rhs);
-bool operator!=(const double& lhs, const Complex& rhs);
-bool operator!=(const Complex& lhs, const double& rhs);
 Complex operator+(const Complex& lhs, const Complex& rhs);
-Complex operator+(const double& lhs, const Complex& rhs);
-Complex operator+(const Complex& lhs, const double& rhs);
+Complex operator+(const Complex& lhs, const double rhs);
+Complex operator+(const double lhs, const Complex& rhs);
 Complex operator-(const Complex& lhs, const Complex& rhs);
-Complex operator-(const double& lhs, const Complex& rhs);
-Complex operator-(const Complex& lhs, const double& rhs);
+Complex operator-(const Complex& lhs, const double rhs);
+Complex operator-(const double lhs, const Complex& rhs);
+
 Complex operator*(const Complex& lhs, const Complex& rhs);
-Complex operator*(const double& lhs, const Complex& rhs);
+
+
 Complex operator*(const Complex& lhs, const double rhs);
+
+Complex operator*(const double lhs, const Complex& rhs);
+
 Complex operator/(const Complex& lhs, const Complex& rhs);
-Complex operator/(const double& lhs, const Complex& rhs);
-Complex operator/(const Complex& lhs, const double& rhs);
-std::ostream& operator<<(std::ostream& ostrm, const Complex& rhs);
-std::istream& operator>>(std::istream& istrm, Complex& rhs);
-bool testParse(const std::string& str);
+
+Complex operator/(const Complex& lhs, const double rhs);
+
+Complex operator/(const double lhs, const Complex& rhs);
+
+inline std::ostream& operator<<(std::ostream& ostrm, const Complex& rhs);
+inline std::istream& operator>>(std::istream& istrm, Complex& rhs);
+bool testparse(const std::string& str);
+
 #endif
