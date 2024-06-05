33dc5eaefd230d08a6a74b977861a21f9e54cfd5
DearByy <145282579+DearByy@users.noreply.github.com>
2024-06-05 10:48:53 +0300

    Update queuelst.hpp

--- prj.lab/queuelst/queuelst.hpp
+++ prj.lab/queuelst/queuelst.hpp
@@ -1,85 +1,34 @@
-#ifndef QUEUELSTHPP24022024
-#define QUEUELSTHPP24022024
+#pragma once
+#ifndef QUEUELST_HPP
+#define QUEUELST_HPP
 
 #include <complex/complex.hpp>
-#include <iostream>
+#include <cstddef>
 
 class QueueLst {
-protected:
-    struct Node {
-        Node* next_{nullptr};
-        Complex data_;
+ public:
+  QueueLst() = default;
+  QueueLst(const QueueLst&);
+  QueueLst(QueueLst&&) noexcept;
+  QueueLst& operator=(const QueueLst&);
+  QueueLst& operator=(QueueLst&&) noexcept;
+  ~QueueLst();
 
-        Node() = default;
-        Node(Node* const next) 
-            : next_{next}
-        {
-        }
-        Node(const Complex& data) 
-            : data_{data}
-        {
-        }
-        ~Node() = default;
-    };
-    Node* first_{nullptr};
-    Node* last_{nullptr};
-public:
-    QueueLst() = default;
-    ~QueueLst() {
-        while (first_ != nullptr) {
-            Node* temp = first_;
-            first_ = temp->next_;
-            delete temp;
-        }
-        last_ = nullptr;
-    }
-    QueueLst(const QueueLst& rhs) {
-        Node* rhs_first = rhs.first_;
-        while (rhs_first != nullptr) {
-            this->Push(rhs_first->data_);
-            rhs_first = rhs_first->next_;
-        }
-    }
-    QueueLst(QueueLst&& rhs) {
-        Node* tempfirst_ = first_;
-        Node* templast_ = last_;
-        first_ = rhs.first_;
-        last_ = rhs.last_;
-        rhs.first_ = tempfirst_;
-        rhs.last_ = templast_;
-    }
-    QueueLst& operator=(const QueueLst& rhs) {
-        if (this != &rhs) {
-            while (first_ != nullptr) {
-                Node* temp = first_;
-                first_ = temp->next_;
-                delete temp;
-            }
-            last_ = nullptr;
-            Node* rhs_first = rhs.first_;
-            while (rhs_first != nullptr) {
-                this->Push(rhs_first->data_);
-                rhs_first = rhs_first->next_;
-            }
-        }
-        return *this;
-    }
-    QueueLst& operator=(QueueLst&& rhs) {
-        Node* tempfirst_ = first_;
-        Node* templast_ = last_;
-        first_ = rhs.first_;
-        last_ = rhs.last_;
-        rhs.first_ = tempfirst_;
-        rhs.last_ = templast_;
-        return *this;
-    }
+  bool IsEmpty() const noexcept;
+  void Pop() noexcept;
+  void Push(const Complex& val);
+  Complex& Top();
+  const Complex& Top() const;
+  void Clear() noexcept;
 
-    virtual void Push(const Complex&) noexcept;
-    void Pop() noexcept;
-    Complex& Top();
-    const Complex& Top() const;
-    bool IsEmpty();
-    void Clear() noexcept; 
+ private:
+  struct Node {
+    Complex val;
+    Node* next = nullptr;
+  };
+
+  Node* head_ = nullptr;
+  Node* tail_ = nullptr;
 };
 
-#endif //QUEUELSTHPP24022024
+#endif
